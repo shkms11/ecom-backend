b264a7c7db97d661b4ac9c5f53defd130f2ebe47
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-02-18 23:41:42 +0600

    remove domainevent from idomainevent

--- src/Core/EcommerceAPI.Domain/Common/IDomainEvent.cs
+++ src/Core/EcommerceAPI.Domain/Common/IDomainEvent.cs
@@ -1,11 +1,3 @@
 namespace EcommerceAPI.Domain.Common;
 
-public interface IDomainEvent
-{
-    DateTime OccurredOn { get; }
-}
-
-public abstract class DomainEvent : IDomainEvent
-{
-    public DateTime OccurredOn { get; } = DateTime.UtcNow;
-}
+public interface IDomainEvent { }
