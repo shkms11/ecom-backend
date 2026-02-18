21cfd44303deda349faedc543ae7ca5ce37fd2e3
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-02-18 23:47:23 +0600

    complete value Object

---
+++ src/Core/EcommerceAPI.Domain/Common/ValueObject.cs
@@ -0,0 +1,45 @@
+namespace EcommerceAPI.Domain.Common;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+            return false;
+
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(
+                1,
+                (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return current * 23 + (obj?.GetHashCode() ?? 0);
+                    }
+                }
+            );
+    }
+
+    public static bool operator ==(ValueObject? a, ValueObject? b)
+    {
+        if (a is null && b is null)
+            return true;
+        if (a is null || b is null)
+            return false;
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(ValueObject? a, ValueObject? b)
+    {
+        return !(a == b);
+    }
+}
